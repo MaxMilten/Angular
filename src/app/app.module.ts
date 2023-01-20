6c1d0ef5c5d3eabfadcb3ee03f4105fe2fcd3771
kevin Murillo <kevinricardomc@gmail.com>
2023-01-19 20:46:09 -0500

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ListaComponent } from './component/lista/lista.component';
+import { LoginComponent } from './component/login/login.component';
+import { LoginAdministradorComponent } from './component/login-administrador/login-administrador.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { StudentsComponent } from './component/students/students.component';
+import { ToolbarComponent } from './component/toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaComponent,
+    LoginComponent,
+    LoginAdministradorComponent,
+    NavbarComponent,
+    StudentsComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule
